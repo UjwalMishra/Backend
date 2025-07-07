e95229034a64b9abd3ec2b9a3f840046a09fa3d0
Ujwal <ujwalcse01@gmail.com>
2025-07-07 18:28:59 +0530

    WS basics -> ping-pong backend

---
+++ websockets-basics/dist/index.js
@@ -0,0 +1,15 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+//creating web server
+const ws_1 = require("ws");
+const PORT = 8080;
+//initializing
+const wss = new ws_1.WebSocketServer({ port: PORT });
+wss.on("connection", (socket) => {
+    console.log("User connected");
+    socket.on("message", (e) => {
+        if (e.toString() === "Ping!") {
+            socket.send("Pong!");
+        }
+    });
+});
---
+++ websockets-basics/src/index.ts
@@ -0,0 +1,15 @@
+//creating web server
+import { WebSocketServer } from "ws";
+
+const PORT = 8080;
+//initializing
+const wss = new WebSocketServer({ port: PORT });
+
+wss.on("connection", (socket) => {
+  console.log("User connected");
+  socket.on("message", (e) => {
+    if (e.toString() === "Ping!") {
+      socket.send("Pong!");
+    }
+  });
+});
