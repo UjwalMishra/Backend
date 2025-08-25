16da3c0529e8218c06fa9a976dc80a5431e5c04a
Ujwal <ujwalcse01@gmail.com>
2025-08-25 17:26:33 +0530

    prisma - using prisma client to send and recieve data with db, added relation b/w two tables

--- prisma-basics/be/src/index.ts
+++ prisma-basics/be/src/index.ts
@@ -1,3 +1,46 @@
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from "@prisma/client"; // just like importing mongodb
 
+//creating instance
 const client = new PrismaClient();
+
+async function createUser() {
+  const res = await client.user2.create({
+    data: {
+      username: "Ujwal",
+      password: "123456",
+      age: 21,
+      city: "Amritsar",
+    },
+  });
+  console.log(res);
+}
+
+// createUser();
+
+async function createTodo() {
+  const res = await client.todo.create({
+    data: {
+      title: "DSA",
+      description: "Complete 2 questions",
+      done: false,
+      userId: 1,
+    },
+  });
+  console.log(res);
+}
+
+// createTodo();
+
+async function getUserDetails() {
+  const res = await client.user2.findUnique({
+    where: {
+      id: 1,
+    },
+    include: {
+      todos: true,
+    },
+  });
+  console.log(res);
+}
+
+getUserDetails();
